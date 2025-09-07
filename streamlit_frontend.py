94f0a516f1d1fe6b2e9afe2cdcf2c08366ad926a
gkdpdf <gauravkumrd2018@gmail.com>
2025-09-07 08:16:14 +0530

    with streaming

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -18,14 +18,28 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    result = workflow.invoke(
-        {"messages": [HumanMessage(content=user_input)]},
-        config=config
-    )
-    ai_message = result['messages'][-1].content
-    st.session_state['message_history'].append({'role':'assistant','content':ai_message})
+    # result = workflow.invoke(
+    #     {"messages": [HumanMessage(content=user_input)]},
+    #     config=config
+    # )
+    # ai_message = result['messages'][-1].content
+    # st.session_state['message_history'].append({'role':'assistant','content':ai_message})
   
+    # with st.chat_message('assistant'):
+    #     st.text(ai_message)
+
     with st.chat_message('assistant'):
-        st.text(ai_message)
+
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in workflow.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= {'configurable': {'thread_id': 'thread-1'}},
+                stream_mode= 'messages'
+            )
+        )
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+
+    
 
 
